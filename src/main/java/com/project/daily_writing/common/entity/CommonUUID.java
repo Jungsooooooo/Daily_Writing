eb9a5a1bd6c166143228aedc0ee24e56f8c23525
DESKTOP-7L8OMEK\hr <tnrlaos@gmail.com>
2023-11-10 14:22:48 +0900

    UUID 깨짐 현상 수정 중

--- src/main/java/com/project/daily_writing/common/entity/CommonUUID.java
+++ src/main/java/com/project/daily_writing/common/entity/CommonUUID.java
@@ -2,24 +2,24 @@ package com.project.daily_writing.common.entity;
 
 import java.util.UUID;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
 import javax.persistence.Table;
 
 import lombok.Getter;
-import lombok.Setter;
 
 @Entity
 @Getter
 @Table(name= "CommonUUID")
 @Inheritance(strategy = InheritanceType.JOINED)
-public abstract class CommonUUID {
+public class CommonUUID {
 	
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(generator="uuid2")
+	@Column(columnDefinition = "VARBINARY(16)")
 	private UUID uuid;
 }
