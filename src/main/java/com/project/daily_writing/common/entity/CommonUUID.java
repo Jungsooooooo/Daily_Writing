689b7af207c32eb75556d1bda3553ff1e3906165
DESKTOP-7L8OMEK\hr <tnrlaos@gmail.com>
2023-11-13 14:19:13 +0900

    uuid -> long 으로 primary key 수정

--- src/main/java/com/project/daily_writing/common/entity/CommonUUID.java
+++ src/main/java/com/project/daily_writing/common/entity/CommonUUID.java
@@ -5,10 +5,13 @@ import java.util.UUID;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
 import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
 
 import lombok.Getter;
 
@@ -19,7 +22,8 @@ import lombok.Getter;
 public class CommonUUID {
 	
 	@Id
-	@GeneratedValue(generator="uuid2")
-	@Column(columnDefinition = "VARBINARY(16)")
-	private UUID uuid;
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "ID")
+	private Long id;
+	
 }
